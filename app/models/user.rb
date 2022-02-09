4e465432b57d73e4b16e48b3d60182023a99e789
Mitchell <Hmitchell.hall@gmail.com>
2022-02-09 09:49:32 -0600

    added has_secure_password to User model and authorize action in application controller as a before_action on all actions

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,3 @@
 class User < ApplicationRecord
-    def create
-        user = User.create(user_params)
-        session[:user_id] = user.id
-        render json :user, status: :created
-    end
-
-
-    private
-    def user_params
-        params.permit()
-    end
+    has_secure_password
 end
